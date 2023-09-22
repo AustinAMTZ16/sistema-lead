ecbf28df25e693511c859986c3e54ae563b70535
AustinAMTZ16 <austintv52@gmail.com>
2023-09-21 20:45:14 -0600

    Prueba Mail()

--- functions/RecuperarPassword.php
+++ functions/RecuperarPassword.php
@@ -4,5 +4,5 @@
 
     $correo = $_POST['dominioB2B'];
     mail($correo, "Hotel Casa de Piedra", 'Su reservación está listo un asesor se podrá en contacto.');
-    mail('soporte@hotelcasadepiedra.com', "Hotel Casa de Piedra", 'El sistema detectó una reservación por favor de revisar el sistema de gestión del hotel');
+    mail('soporte@engranetmx.com', "Hotel Casa de Piedra", 'El sistema detectó una reservación por favor de revisar el sistema de gestión del hotel');
 ?>
