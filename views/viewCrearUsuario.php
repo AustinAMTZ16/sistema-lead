662d249a4610d4c248c9a160e6ac1c5baf6fc71f
AustinAMTZ16 <austintv52@gmail.com>
2023-09-21 22:17:27 -0600

    Agregar Prooverdor

--- views/viewCrearUsuario.php
+++ views/viewCrearUsuario.php
@@ -20,19 +20,19 @@
                             <h3>Crea tu perfil MexiClientes</h3>
                             <div class="mb-3">
                                 <label for="text" class="form-label ">Nombre del negocio: </label>
-                                <input type="text" class="form-control" name="nombreEmpresa" id="nombreEmpresa" placeholder="name@example.com" required>
+                                <input type="text" class="form-control" name="nombreEmpresa" id="nombreEmpresa" placeholder="ejemplo: engranet" required>
                             </div>
                             <div class="mb-3">
                                 <label for="text" class="form-label ">Pagina web del negocio: </label>
-                                <input type="text" class="form-control" name="dominioB2B" id="dominioB2B" placeholder="name@example.com" required>
+                                <input type="text" class="form-control" name="dominioB2B" id="dominioB2B" placeholder="ejemplo: engranetmx.com" required>
                             </div>
                             <div class="mb-3">
                                 <label for="email" class="form-label ">Correo electrónico del negocio: </label>
-                                <input type="email" class="form-control" name="correo" id="correo" placeholder="name@example.com" required>
+                                <input type="email" class="form-control" name="correo" id="correo" placeholder="ejemplo: name@example.com" required>
                             </div>
                             <div class="mb-3">
                                 <label for="text" class="form-label ">Giro del negocio: </label>
-                                <input type="text" class="form-control" name="giroDominio" id="giroDominio" placeholder="name@example.com" required>
+                                <input type="text" class="form-control" name="giroDominio" id="giroDominio" placeholder="ejemplo: Marketing digital" required>
                             </div>
                             <div class="mb-3">
                                 <label for="password" class="form-label ">Contraseña del perfil</label>
