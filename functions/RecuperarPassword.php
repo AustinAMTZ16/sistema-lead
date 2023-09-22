039242dbae5629037d325fe78d37f6b570bdaaaa
AustinAMTZ16 <austintv52@gmail.com>
2023-09-21 21:01:23 -0600

    Prueba Mail()

--- functions/RecuperarPassword.php
+++ functions/RecuperarPassword.php
@@ -1,8 +1,8 @@
 <?php
-    require_once './../connection/conecionPDO.php'; 
+    require_once './../connection/conexion.php'; 
 
 
     $correo = $_POST['correo'];
-    mail($_POST['dominioB2B'], "Hotel Casa de Piedra", 'Su reservación está listo un asesor se podrá en contacto.');
-    mail('soporte@engranetmx.com', "Hotel Casa de Piedra", 'El sistema detectó una reservación por favor de revisar el sistema de gestión del hotel');
+    mail($_POST['correo'], "Hotel Casa de Piedra", 'Su reservación está listo un asesor se podrá en contacto.');
+
 ?>
--- views/viewRecuperarPassword.php
+++ views/viewRecuperarPassword.php
@@ -1,8 +1,8 @@
 <?php
-  //session_start();
+  session_start();
   require_once './../functions/RecuperarPassword.php';
   // Cerrar la conexión
-  //$conn->close();
+  $conn->close();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,19 +16,17 @@
             <div class="col-10 col-sm-6">
                 <div class="container text-center">
                     <div class="row justify-content-center">
-                        <form method="POST">
+                        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                             <h3>Recupera tu perfil MexiClientes</h3>
                             <div class="mb-3">
                                 <label for="email" class="form-label ">Dirección de correo electrónico </label>
-                                <input type="email" autocomplete="off" class="form-control" name="correo" id="correo" value="<?php if(isset($_POST['correo'])) echo $_POST['correo']?>
-                                
-                                " require placeholder="name@example.com">
+                                <input type="email" class="form-control" name="correo" id="correo" require placeholder="name@example.com">
                             </div>
                             <div class="d-grid">
                                 <button class="btn btn-outline-dark" name="md_insert" type="submit">Recuperar cuenta</button>
                             </div>
                             <div class="d-grid">
-                                <button class="btn btn-outline-dark" type="submit" onclick="window.location.href='../index.php'">Regresar al Menu</button>
+                                <button class="btn btn-outline-dark" type="submit"><a href="../index.php">Regresar al Menu</a></button>
                             </div>
                         </form>
                     </div>
