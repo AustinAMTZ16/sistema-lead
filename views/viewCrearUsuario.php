4eee05b83c6d4df0c4acd9149aae730b4c00f963
AustinAMTZ16 <austintv52@gmail.com>
2023-09-21 22:14:36 -0600

    Agregar Prooverdor

--- views/viewCrearUsuario.php
+++ views/viewCrearUsuario.php
@@ -1,3 +1,9 @@
+<?php
+  session_start();
+  require_once './../functions/CrearUsuario.php';
+  // Cerrar la conexión
+  $conn->close();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,32 +19,34 @@
                         <form method="POST" action="<?php echo htmlspecialchars($_SERVER    ["PHP_SELF"]); ?>">
                             <h3>Crea tu perfil MexiClientes</h3>
                             <div class="mb-3">
-                                <label for="email" class="form-label ">Usuario </label>
-                                <input type="text" class="form-control" name="usuario" id="usuario" placeholder="name@example.com" required>
+                                <label for="text" class="form-label ">Nombre del negocio: </label>
+                                <input type="text" class="form-control" name="nombreEmpresa" id="nombreEmpresa" placeholder="name@example.com" required>
                             </div>
                             <div class="mb-3">
-                                <label for="email" class="form-label ">Usuario </label>
-                                <input type="text" class="form-control" name="usuario" id="usuario" placeholder="name@example.com" required>
+                                <label for="text" class="form-label ">Pagina web del negocio: </label>
+                                <input type="text" class="form-control" name="dominioB2B" id="dominioB2B" placeholder="name@example.com" required>
                             </div>
                             <div class="mb-3">
-                                <label for="email" class="form-label ">Usuario </label>
-                                <input type="text" class="form-control" name="usuario" id="usuario" placeholder="name@example.com" required>
+                                <label for="email" class="form-label ">Correo electrónico del negocio: </label>
+                                <input type="email" class="form-control" name="correo" id="correo" placeholder="name@example.com" required>
                             </div>
                             <div class="mb-3">
-                                <label for="email" class="form-label ">Usuario </label>
-                                <input type="text" class="form-control" name="usuario" id="usuario" placeholder="name@example.com" required>
+                                <label for="text" class="form-label ">Giro del negocio: </label>
+                                <input type="text" class="form-control" name="giroDominio" id="giroDominio" placeholder="name@example.com" required>
                             </div>
                             <div class="mb-3">
-                                <label for="password" class="form-label ">Contraseña</label>
-                                <input type="password" class="form-control" name="contraseña" id="contraseña" placeholder="*******" required>
+                                <label for="password" class="form-label ">Contraseña del perfil</label>
+                                <input type="password" class="form-control" name="password" id="password" placeholder="*******" required>
                             </div>
+                            
                             <div class="d-grid">
-                                <button class="btn btn-outline-dark" type="submit">Iniciar Sesión</button>
+                                <button class="btn btn-outline-dark" name="saveCompany" type="submit">Iniciar Sesión</button>
                             </div>
-                            <div class="d-grid">
-                                <button class="btn btn-outline-dark" type="submit" onclick="window.location.href='../index.php'">Regresar al Menu</button>
-                            </div>
+                            
                         </form>
+                        <div class="d-grid">
+                            <button class="btn btn-outline-dark" type="submit" ><a href="../index.php">Regresar al Menu</a></button>
+                        </div>
                     </div>
                 </div>
             </div>
