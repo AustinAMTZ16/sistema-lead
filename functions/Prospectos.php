93fa59a7ac6a6d52d1b3408e864bd2ef631da423
AustinAMTZ16 <austintv52@gmail.com>
2023-10-07 13:14:12 -0600

    Prueba Plantilla mailSuscripcion

--- functions/Prospectos.php
+++ functions/Prospectos.php
@@ -1,5 +1,6 @@
 <?php
     include '../../connection/dbcon.php';
+    //require '../views/viewMail/mailSuscricion.php';
     /* CAMPOS NECESARIOS PARA CREAR UN PROSPECTO
         idProspecto : 'int : identificador de prospecto', 
         nombre : 'varchar : nombre del prospecto', 
@@ -98,6 +99,79 @@
         $conversacion = $data['conversacion'];
         $correoCliente = $data['correoCliente'];
 
+        $mailSuscripcion = '
+            <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
+                <tr>
+                    <td align="center" style="padding:0;">
+                        <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
+                            <tr>
+                                <td align="center" style="padding:40px 0 30px 0;background:#70bbd9;">
+                                    <img src="https://assets.codepen.io/210284/h1.png" alt="" width="300" style="height:auto;display:block;" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:36px 30px 42px 30px;">
+                                    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                                        <tr>
+                                            <td style="padding:0 0 36px 0;color:#153643;">
+                                                <h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;">Creating Email Magic</h1>
+                                                <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan et dictum, nisi libero ultricies ipsum, posuere neque at erat.</p>
+                                                <p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://www.example.com" style="color:#ee4c50;text-decoration:underline;">In tempus felis blandit</a></p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding:0;">
+                                                <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                                                    <tr>
+                                                        <td style="width:260px;padding:0;vertical-align:top;color:#153643;">
+                                                            <p style="margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><img src="https://assets.codepen.io/210284/left.gif" alt="" width="260" style="height:auto;display:block;" /></p>
+                                                            <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, est nisi libero ultricies ipsum, in posuere mauris neque at erat.</p>
+                                                            <p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://www.example.com" style="color:#ee4c50;text-decoration:underline;">Blandit ipsum volutpat sed</a></p>
+                                                        </td>
+                                                        <td style="width:20px;padding:0;font-size:0;line-height:0;">&nbsp;</td>
+                                                        <td style="width:260px;padding:0;vertical-align:top;color:#153643;">
+                                                            <p style="margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><img src="https://assets.codepen.io/210284/right.gif" alt="" width="260" style="height:auto;display:block;" /></p>
+                                                            <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Morbi porttitor, eget est accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed.</p>
+                                                            <p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://www.example.com" style="color:#ee4c50;text-decoration:underline;">In tempus felis blandit</a></p>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:30px;background:#ee4c50;">
+                                    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
+                                        <tr>
+                                            <td style="padding:0;width:50%;" align="left">
+                                                <p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;">
+                                                    &reg; Someone, Somewhere 2023<br/><a href="http://www.example.com" style="color:#ffffff;text-decoration:underline;">Unsubscribe</a>
+                                                </p>
+                                            </td>
+                                            <td style="padding:0;width:50%;" align="right">
+                                                <table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
+                                                    <tr>
+                                                        <td style="padding:0 0 0 10px;width:38px;">
+                                                            <a href="http://www.twitter.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/tw_1.png" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
+                                                        </td>
+                                                        <td style="padding:0 0 0 10px;width:38px;">
+                                                            <a href="http://www.facebook.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/fb_1.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            ';
+
         $postAgregarProspecto = "INSERT INTO tb_prospecto (nombre, apellidoPaterno, apellidoMaterno,telefono, correo, asunto, mensaje, dominioOrigen, giroDominio, categoriaProspecto, estadoSistema, conversacion)
         VALUES('".$nombre."', '".$apellidoPaterno."', '".$apellidoMaterno."', '".$telefono."', '".$correo."', '".$asunto."', '".$mensaje."', '".$dominioOrigen."', '".$giroDominio."', '".$categoriaProspecto."', '".$estadoSistema."', '".$conversacion."' )";
         $query_run=$conn->query($postAgregarProspecto);
@@ -105,8 +179,8 @@
         if($query_run) {
             if($conn->affected_rows){
                 $res = $query_run;
-                mail($correo, "Gracias por suscribirte", "Gracias por su registro nos estaremos poniendo en contacto lo mas pronto posible.");
-                mail($correoCliente, "Formulario de suscripcion MexiClientes", "Se registro un cliente revisa tu sistema");
+                mail($correo, "Gracias por suscribirte", $mailSuscripcion);
+                mail($correoCliente, "Formulario de suscripcion MexiClientes", $mailSuscripcion);
                 $data = [
                     'status' => 200,
                     'message' => 'Se agrego correctamente el prospecto.',
